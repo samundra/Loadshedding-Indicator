8655a78a8ba8c511536ebd62ca2ec85516bacffb
Samundra Shrestha <samundra.shr@gmail.com>
2016-02-17 11:00:43 +0545

    add indicator icon

--- converter.py
+++ converter.py
@@ -1,111 +1,107 @@
-'''
+"""
 Created on Jun 30, 2013
 
 @author: samundra
-'''
+"""
 
 from gi.repository import Gtk
-from gi.overrides.Gtk import Widget
-from gi.repository import Notify
-from gi.repository import GObject
+# from gi.overrides.Gtk import Widget
+# from gi.repository import Notify
+# from gi.repository import GObject
 from nepali_calendar import NepaliDateConverter
 from ast import literal_eval
+
 
 class ConverterWindow(Gtk.Window):
     ndc = NepaliDateConverter()
     bs2ad = True
     ad2bs = False
+
     def __init__(self):
         Gtk.Window.__init__(self, title="Convert date")
+        self.state = "off"
         self.set_resizable(False)
         self.set_position(Gtk.WindowPosition.CENTER)
         table = Gtk.Table(4, 2, True)
         self.add(table)
 
-        #saved_group_value = "2070-02-10"
-        #self.group_number = 3
-        
-        #dd = '(2070,03,16)'
+        # saved_group_value = "2070-02-10"
+        # self.group_number = 3
+
+        # dd = '(2070,03,16)'
         dd = str(self.ndc.today_en_date())
-        
-        self.txtNpDateField = Gtk.Entry()
-        self.txtNpDateField.set_name('txt_np_date')
-        self.txtNpDateField.set_text("")
-        
-        self.txtEnDateField = Gtk.Entry()
-        self.txtEnDateField.set_name('txt_en_date')
-        self.txtEnDateField.set_text(dd)
-        
-        #BS2AD or AD2BS Choices
-        btnBs2Ad = Gtk.RadioButton.new_with_label_from_widget(None, "BS2AD")
-        btnBs2Ad.connect("toggled", self.on_button_toggled, "nep_to_en")
-        
-        btnAd2Bs = Gtk.RadioButton.new_from_widget(btnBs2Ad)
-        btnAd2Bs.set_label("AD to BS")
-        btnAd2Bs.connect("toggled", self.on_button_toggled, "en_to_nep")
-        btnAd2Bs.set_active(True)
-        
-        btnClose = Gtk.Button(label="Close",name='close')
-        btnClose.connect("clicked", self.on_btn_close)
-        
-        btnSave = Gtk.Button(label="Convert",name='Convert')
-        btnSave.connect("clicked", self.on_btn_convert)
-        
-        #label
-        table.attach(Gtk.Label('B.S. :'),0,1,0,1)
-        #entry
-        table.attach(self.txtNpDateField,1,2,0,1)
-        
-        table.attach(Gtk.Label('A.D. :'),0,1,1,2)
-        table.attach(self.txtEnDateField,1,2,1,2)
-        
-        #Convert Dates Choices
-        table.attach(btnBs2Ad,0,1,2,3)
-        table.attach(btnAd2Bs,1,2,2,3)
-        
-        #close button
-        table.attach(btnClose,0,1,3,4)
-        #save button
-        table.attach(btnSave,1,2,3,4)
-        
+
+        self.txt_np_date_field = Gtk.Entry()
+        self.txt_np_date_field.set_name('txt_np_date')
+        self.txt_np_date_field.set_text("")
+
+        self.txt_en_date_field = Gtk.Entry()
+        self.txt_en_date_field.set_name('txt_en_date')
+        self.txt_en_date_field.set_text(dd)
+
+        # BS2AD or AD2BS Choices
+        btn_bs_2ad = Gtk.RadioButton.new_with_label_from_widget(None, "BS2AD")
+        btn_bs_2ad.connect("toggled", self.on_button_toggled, "nep_to_en")
+
+        btn_ad_2bs = Gtk.RadioButton.new_from_widget(btn_bs_2ad)
+        btn_ad_2bs.set_label("AD to BS")
+        btn_ad_2bs.connect("toggled", self.on_button_toggled, "en_to_nep")
+        btn_ad_2bs.set_active(True)
+
+        btn_close = Gtk.Button(label="Close", name='close')
+        btn_close.connect("clicked", self.on_btn_close)
+
+        btn_save = Gtk.Button(label="Convert", name='Convert')
+        btn_save.connect("clicked", self.on_btn_convert)
+
+        # label
+        table.attach(Gtk.Label('B.S. :'), 0, 1, 0, 1)
+        # entry
+        table.attach(self.txt_np_date_field, 1, 2, 0, 1)
+
+        table.attach(Gtk.Label('A.D. :'), 0, 1, 1, 2)
+        table.attach(self.txt_en_date_field, 1, 2, 1, 2)
+
+        # Convert Dates Choices
+        table.attach(btn_bs_2ad, 0, 1, 2, 3)
+        table.attach(btn_ad_2bs, 1, 2, 2, 3)
+
+        # close button
+        table.attach(btn_close, 0, 1, 3, 4)
+        # save button
+        table.attach(btn_save, 1, 2, 3, 4)
+
     def on_button_toggled(self, button, name):
-        if(name=="nep_to_en"):
-            cnv_np_date = literal_eval(self.txtNpDateField.get_text())
+        if name == "nep_to_en":
+            cnv_np_date = literal_eval(self.txt_np_date_field.get_text())
             self.bs2ad = True
             self.ad2bs = False
-            self.txtEnDateField.set_text(str(self.c_np_date(cnv_np_date)))
-        if(name=="en_to_nep"):
-            cnv_en_date = literal_eval(self.txtEnDateField.get_text())
+            self.txt_en_date_field.set_text(str(self.c_np_date(cnv_np_date)))
+        if name == "en_to_nep":
+            cnv_en_date = literal_eval(self.txt_en_date_field.get_text())
             self.ad2bs = True
             self.bs2ad = False
-            self.txtNpDateField.set_text(str(self.c_en_date(cnv_en_date)))
-            
-        if button.get_active():
-            state = "on"
-        else:
-            state = "off"
-        #print "Button", name, "was turned", state
-    
-    def on_btn_close(self,w):
-        #print int(self.group_number)
-        #self.emit("message_send",int(self.group_number))
-        self.destroy()
-        
-    def c_np_date(self,dd):
-        return self.ndc.bs2ad(dd)
-    
-    def c_en_date(self,dd):
-        return self.ndc.ad2bs(dd)
-    
-    def on_btn_convert(self,widget):
-        if(self.bs2ad== True):
-            cnv_np_date = literal_eval(self.txtNpDateField.get_text())
-            self.txtEnDateField.set_text(str(self.c_np_date(cnv_np_date)))
-        if(self.ad2bs==True):
-            cnv_en_date = literal_eval(self.txtEnDateField.get_text())
-            self.txtNpDateField.set_text(str(self.c_en_date(cnv_en_date)))
+            self.txt_np_date_field.set_text(str(self.c_en_date(cnv_en_date)))
 
-# win = ConverterWindow()
-# win.connect("delete-event", Gtk.main_quit)
-# win.show_all()
-# Gtk.main()    
+        if button.get_active():
+            self.state = "on"
+            # print "Button", name, "was turned", state
+
+    def on_btn_close(self, w):
+        # print int(self.group_number)
+        # self.emit("message_send",int(self.group_number))
+        self.destroy()
+
+    def c_np_date(self, dd):
+        return self.ndc.bs2ad(dd)
+
+    def c_en_date(self, dd):
+        return self.ndc.ad2bs(dd)
+
+    def on_btn_convert(self, widget):
+        if self.bs2ad:
+            cnv_np_date = literal_eval(self.txt_np_date_field.get_text())
+            self.txt_en_date_field.set_text(str(self.c_np_date(cnv_np_date)))
+        if self.ad2bs:
+            cnv_en_date = literal_eval(self.txt_en_date_field.get_text())
+            self.txt_np_date_field.set_text(str(self.c_en_date(cnv_en_date)))
--- main.py
+++ main.py
@@ -4,194 +4,192 @@ Created on Jun 17, 2013
 @author: samundra
 '''
 from gi.repository import Gtk
-from gi.overrides.Gtk import Widget
+# from gi.overrides.Gtk import Widget
 from nepali_calendar import NepaliDateConverter
 from gi.repository import AppIndicator3 as AppIndicator
-from preference import PreferenceWindow
+from preference import PreferenceWindow as preference_window
 from converter import ConverterWindow
 import datetime
 import json
-from xml.dom.minidom import parseString
+import signal
+import os
+# from xml.dom.minidom import parseString
 import subprocess
- 
+
+
 class LoadShedding:
     ind = None
     routines = []
     menu = None
-    
+
     def __init__(self):
-        self.ind = AppIndicator.Indicator.new("loadshedding-routine", 
-                                              "tray-message", 
+        self.ind = AppIndicator.Indicator.new("loadshedding-routine",
+                                              # "tray-message",
+                                              os.path.abspath("icon.png"),
                                               AppIndicator.IndicatorCategory.APPLICATION_STATUS)
         self.ind.set_status(AppIndicator.IndicatorStatus.ACTIVE)
         self.ind.set_attention_icon("indicator-messages-new")
-        #self.ind.connect("scroll-event", self.scroll)
+        # self.ind.connect("scroll-event", self.scroll)
         self.rebuild_menu()
-    
+
     def clear_menu_items(self):
         m_childrens = self.menu.get_children()
         ppos = 0;
         for c in m_childrens:
-            ppos+=1
-            if(ppos in [5,6,7]):
-#                print ppos
- #               print c
+            ppos += 1
+            if (ppos in [5, 6, 7]):
                 self.menu.remove(c)
                 c.destroy()
-        
-        # have to give indicator a menu
-#     def scroll(self,aai,ind,steps):
-#         print "hello"
 
     def rebuild_menu(self):
         self.menu = Gtk.Menu()
         self.clear_menu_items()
-        
+
         # you can use this menu item for experimenting
         item = Gtk.MenuItem()
         item.set_label("Preference")
         item.connect("activate", self.handler_menu_preference)
         item.show()
         self.menu.append(item)
-        
+
         cdate = Gtk.MenuItem()
         cdate.set_label("Convert Date")
-        cdate.connect("activate", self.window_convert_date)
+        cdate.connect("activate", self.handler_convert_date)
         cdate.show()
         self.menu.append(cdate)
 
         sep1 = Gtk.SeparatorMenuItem()
         sep1.show()
         self.menu.append(sep1)
-        
+
         # Append the actual routine here
         self.add_dy_menu()
-        
+
         # this is for exiting the app
         sep2 = Gtk.SeparatorMenuItem()
         sep2.show()
         self.menu.append(sep2)
-        
+
         item = Gtk.MenuItem()
         item.set_label("Update Schedule")
         item.connect("activate", self.update_schedule)
         item.show()
-        
+
         self.menu.append(item)
         self.menu.show()
-        
+
         # this is for exiting the app
         sep3 = Gtk.SeparatorMenuItem()
         sep3.show()
         self.menu.append(sep3)
-        
+
         item = Gtk.MenuItem()
         item.set_label("Exit")
         item.connect("activate", self.handler_menu_exit)
         item.show()
-        
+
         self.menu.append(item)
         self.menu.show()
-        
+
         self.ind.set_title("Menu title")
         self.ind.set_menu(self.menu)
-        
-        
+
         m_childrens = self.menu.get_children()
-        print 'length '+ str(len(m_childrens))
+        print 'length ' + str(len(m_childrens))
         pos = 0;
         for c in m_childrens:
-            pos+=1
-            if(pos in [5,6,7]) and (len(m_childrens)>9):
+            pos += 1
+            if (pos in [5, 6, 7]) and (len(m_childrens) > 9):
                 self.menu.remove(c)
                 c.destroy()
-        
-    def update_schedule(self,evt):
+
+    def update_schedule(self, evt):
         upd_string = 'cd batti && ./main.sh -u && ./main.sh -x > ../t.xml'
-#         upd_sch = subprocess.Popen(upd_string, shell=True)
+        #         upd_sch = subprocess.Popen(upd_string, shell=True)
         process = subprocess.Popen(upd_string, shell=True, stdout=subprocess.PIPE)
         print process.communicate()
-#         print "update scheudle"
-        
-    def window_convert_date(self,evt):
+
+    #         print "update scheudle"
+
+    def handler_convert_date(self, evt):
         cd = ConverterWindow()
         cd.show_all()
-            
+
     def add_dy_menu(self):
-        ''' Adds the dynamic menu generated from xml '''
-#         now = datetime.datetime.now()
-#         labelDay = now.strftime("%Y-%m-%d")+" ("+ now.strftime("%A")+")"
+        """ Adds the dynamic menu generated from xml """
+        #         now = datetime.datetime.now()
+        #         labelDay = now.strftime("%Y-%m-%d")+" ("+ now.strftime("%A")+")"
         np_date_converter = NepaliDateConverter()
         np_date = np_date_converter.contents_func()
         curDate = Gtk.MenuItem(label=np_date)
         curDate.show()
-        
+
         self.menu.append(curDate)
 
-        if(self.parse_xml()):
-            for items in self.routines:                
+        if (self.parse_xml()):
+            for items in self.routines:
                 itema = Gtk.MenuItem(label=items)
                 itema.show()
                 self.menu.append(itema)
-                itema = None
-                
+                # itema = None
+
     def get_group_number(self):
         configs = json.load(open("config.txt"))
         return configs['GROUP']
-    
+
     def load_routine(self, widget):
         self.parse_xml()
-    
+
     def parse_xml(self):
         MY_GROUP = self.get_group_number()  # Group number
-        #print MY_GROUP
+        # print MY_GROUP
         ''' Parses the routine xml and keeps the routine in memory '''
         from xml.dom import minidom
-        
+
         self.xmldoc = minidom.parse('routine.xml')
-        itemlist = self.xmldoc.getElementsByTagName('group') 
-        self.routines.append("GROUP : "+str(MY_GROUP))
-        for s in itemlist :
+        itemlist = self.xmldoc.getElementsByTagName('group')
+        self.routines.append("GROUP : " + str(MY_GROUP))
+        for s in itemlist:
             groupName = int(s.getAttribute('name'))
-            
-            if(groupName==MY_GROUP):
+
+            if (groupName == MY_GROUP):
                 day = s.getElementsByTagName('day')
-                
+
                 for dt in day:
 
                     dayName = dt.getAttribute('name').lower()
-                    
+
                     now = datetime.datetime.now()
                     currDay = now.strftime("%A").lower()
 
-                    if(currDay==dayName):
+                    if (currDay == dayName):
                         self.routines.append(dt.childNodes[1].firstChild.nodeValue)
                         self.routines.append(dt.childNodes[3].firstChild.nodeValue)
-                        return True 
+                        return True
         return False
-                
+
     def handler_menu_exit(self, evt):
         Gtk.main_quit()
-                
+
     def handler_menu_preference(self, evt):
-        pref = PreferenceWindow()
-        pref.connect("message_send",self.on_pref_close)
-#         pref.connect("delete-event", Gtk.main_quit)
+        pref = preference_window()
+        pref.connect("message_send", self.on_pref_close)
+        #         pref.connect("delete-event", Gtk.main_quit)
         pref.show_all()
-        
-    def on_pref_close(self,evt,data=None):
-        #print 'reloading menu'
-        #Reload the menu
+
+    def on_pref_close(self, evt, data=None):
         self.rebuild_menu()
-        
+
     def main(self):
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
         Gtk.main()
-    
+
     def menu_reload(self):
         print 'menu reload'
+
 
 if __name__ == "__main__":
     lshd = LoadShedding()
     lshd.main()
 
-#LoadShedding.main()       
+# LoadShedding.main()
